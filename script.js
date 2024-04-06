b41fe2f84010811431eac0e93d28dc0e60a0ec3f
patrickthe1 <mugishapatrick730@gmail.com>
2024-04-06 15:27:57 +0200

    Create a function that plays a single round

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
- let playerSelection = prompt("Enter your choice");
+ 
 
 function getComputerChoice(){
     const choices = ["Rock","Paper","Scissors"];
@@ -7,6 +7,29 @@ function getComputerChoice(){
     return choices[randomIndex];
 }
 
-console.log(getComputerChoice());
 
-function playRound(playerSelection,computerSelection){}
+
+
+
+
+function playRound(playerSelection,computerSelection){
+ if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+    return "It is a tie";
+ } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase()=== "paper")
+ {
+    return "You lost";
+ } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase()=== "rock")
+ {
+    return "You won";
+ } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase()=== "rock"){
+    return "You lost!";
+ }  else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase()=== "scissors"){
+    return "You won!";
+ } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase()=== "scissors"){
+    return "You lost!";
+ } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase()=== "paper"){
+    return "You won!";
+ } else {
+    return "unexpected token";
+ }
+}
