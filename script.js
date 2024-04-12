76110e80b603b73ddadce4d9d7954338856e0763
patrickthe1 <mugishapatrick730@gmail.com>
2024-04-12 15:55:39 +0200

    create a function that plays the game five times with a loop

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
- 
-
 function getComputerChoice(){
     const choices = ["Rock","Paper","Scissors"];
     const randomIndex =  Math.floor(Math.random() * choices.length);
@@ -8,28 +6,70 @@ function getComputerChoice(){
 }
 
 
-
-
+ 
+function getPlayerSelection(){
+   let choice = prompt("Make your choice").toLowerCase();
+   return choice;
+}
 
 
 function playRound(playerSelection,computerSelection){
  if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-    return "It is a tie";
+    console.log( "Draw")
+    return 0;
  } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase()=== "paper")
  {
-    return "You lost";
+    console.log( "You lost")
+    return 2;
  } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase()=== "rock")
  {
-    return "You won";
+    console.log( "You won")
+    return 1;
  } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase()=== "rock"){
-    return "You lost!";
+    console.log( "You lost!")
+    return 2;
  }  else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase()=== "scissors"){
-    return "You won!";
+    console.log( "You won!")
+    return 1;
  } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase()=== "scissors"){
-    return "You lost!";
+    console.log( "You lost!")
+    return 2;
  } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase()=== "paper"){
-    return "You won!";
+    console.log( "You won!")
+    return 1;
  } else {
-    return "unexpected token";
+    console.log( "unexpected token");
  }
 }
+
+
+function playGame(){
+let playerScore = 0;
+let CompScore = 0;
+
+for(i=1; i <= 5; i++){
+    let compChoice = getComputerChoice();
+    let playerChoice = getPlayerSelection();
+    let result = playRound(playerChoice,compChoice);
+    
+    if(result === 1){
+      playerScore++;
+    } else if( result === 2){
+      CompScore++;
+    } else{
+      playerScore++;
+      CompScore++;
+    }
+}
+
+if(playerScore > CompScore){
+   console.log(`Congratulations, you are the 1 with: ${playerScore} to ${CompScore}`);
+} else if(playerScore < CompScore){
+   console.log(`you have lost you fuck with score:${playerScore} to ${CompScore}`);
+} else {
+   console.log(`Its a tie:${playerScore} to ${CompScore}`)
+}
+}
+
+console.log("Hello World");
+playGame();
